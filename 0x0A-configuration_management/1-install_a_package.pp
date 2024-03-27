00185fbb5eb437da685631b2c9204b4d10e1ce31
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-03-27 03:59:19 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
-#Using Puppet, install flask from pip3.
-#Requirements:Install flask, Version must be 2.1.0
-
-package { 'python3-pip':  # Ensure pip3 (Python package installer) is installed
-  ensure => installed,
-}
-
-exec { 'install_flask':  # Use exec resource to execute pip3 command to install Flask
-  command => '/usr/bin/pip3 install Flask==2.1.0',  # Install Flask version 2.1.0
-  path    => ['/usr/bin'],  # Set the path to include pip3
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',  # Check if Flask is already installed at the desired version
-  require => Package['python3-pip'],  # Ensure python3-pip package is installed before installing Flask
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+	ensure   => '2.1.0',
+	provider => 'pip3'
 }
