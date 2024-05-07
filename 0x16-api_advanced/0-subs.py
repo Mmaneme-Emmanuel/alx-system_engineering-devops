0b04b99e324e34f7d1c566b5892ffb169a82aa82
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-05-08 00:07:25 +0100

    task 0 - 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
-#!/usr/bin/python3
-"""
-Script that queries the Reddit API and returns the number of subscribers
-"""
 import requests
 
-
 def number_of_subscribers(subreddit):
+    """Return the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "My Custom User Agent"}  # Update with your custom User-Agent
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        if 'data' in data and 'subscribers' in data['data']:
+            return data['data']['subscribers']
+        else:
+            # Invalid subreddit
+            return 0
     else:
+        # Something went wrong or the subreddit is invalid
         return 0
