75c7270e17409872ba9cc8dc4bba415a3b90f788
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-05-07 23:47:44 +0100

    task 0 - 3

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests
+
+
+def top_ten(subreddit):
+    """Print the titles of the first 10 hot posts of a subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+    else:
+        print(None)
