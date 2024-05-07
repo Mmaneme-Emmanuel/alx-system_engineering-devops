28072f2bd7931c793425f373bd07673db49e00e6
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-05-07 23:56:56 +0100

    task 0 - 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,8 +6,8 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"User-Agent": "Mozilla/5.0"}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
