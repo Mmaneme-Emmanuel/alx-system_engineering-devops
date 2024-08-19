72dabbd5b9e4ff731cd78d6ff7524a69bb5c0534
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-19 12:28:08 +0100

    task 0 done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
 import requests
-import sys
+from sys import argv
+
+
+def fetch_employee_todos(employee_id):
+    url = "https://jsonplaceholder.typicode.com/"
+    response = requests.get(url + "users/{}".format(employee_id))
+    employee = response.json()
+
+    todo_response = requests.get(url + "todos?userId={}".format(employee_id))
+    todos = todo_response.json()
+
+    completed = [todo for todo in todos if todo.get("completed")]
+
+    # Print the result
+    print("{} is done with {}/{}".format(employee['name'],
+          len(completed), len(todos)))
+
+    for task in completed:
+        print("\t {}".format(task['title']))
+
 
 if __name__ == "__main__":
-    # Get the employee ID from command-line arguments
-    employee_id = sys.argv[1]
-
-# Fetch employee details
-url = "https://jsonplaceholder.typicode.com/"  # Assuming this is the base URL
-response = requests.get(url + "users/{}".format(employee_id))
-employee = response.json()
-
-# Fetch todos for the employee
-todo_response = requests.get(url + "todos?userId={}".format(employee_id))
-todos = todo_response.json()
-
-# Collect completed tasks
-completed = []
-
-for todo in todos:
-    if todo.get("completed"):
-        completed.append(todo)
-
-# Print the result
-print("{} is done with {}/{}".format(employee['name'],
-      len(completed), len(todos)))
-
-for task in completed:
-    print("\t {}".format(task['title']))
+    employee_id = argv[1]
+    fetch_employee_todos(employee_id)
