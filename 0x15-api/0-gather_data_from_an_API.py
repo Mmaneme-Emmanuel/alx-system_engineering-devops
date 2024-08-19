eea5784ac3bf01bb2f2afefc1b42f3073d6b5228
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-19 13:50:27 +0100

    task 0 done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,29 +6,31 @@ from sys import argv
 def fetch_employee_todos(employee_id):
     try:
         url = "https://jsonplaceholder.typicode.com/"
-        user_info = requests.get(url + f"users/{employee_id}")
-        response = user_info.json()
-        employee_name = response["name"]
+        user_datas = requests.get(url + f"users/{employee_id}")
+        user_data = user_datas.json()
+        employee_name = user_data["name"]
 
         """getting the todolist for the employee"""
         todo_list = requests.get(url + f"todos?userId={employee_id}")
         json_converted_todos = todo_list.json()
 
-        total_task = (json_converted_todos)
+        total_task = len(json_converted_todos)
         task_done = [task for task in json_converted_todos
                      if task["completed"]]
         num_of_task_done = len(task_done)
 
         # Print the result
-        print(f"Employee {employee_name} is done with task",
-              f"{num_of_task_done}/{task_done}:")
+        print(f"Employee {employee_name} is done with task("
+              f"{num_of_task_done}/{total_task}):")
 
         for task in task_done:
             print(f"\t {task['title']}")
     except Exception as e:
-        print(f"an error occured {e}")
+        print(f"an error occured: {e}")
 
 
 if __name__ == "__main__":
-    employee_id = argv[1]
-    fetch_employee_todos(employee_id)
+    if len(argv) != 2:
+        print("usage: Script <employee_id>")
+    else:
+        fetch_employee_todos(argv[1])
