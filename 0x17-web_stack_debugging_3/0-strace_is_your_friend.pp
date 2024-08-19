0c5ccbb53ea414c8f494135cb5d570f445bd2364
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-19 03:13:03 +0100

    task done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 
 #The mistyped .phpp has been editted in the /var/www/html/wp-settings.php file
 
-exec { 'fix-wordpress-server-error';
-	command => 'sed -i s/phpp/php/g /var/www/html/up-settings.php',
-path => '/usr/bin/:/bin/',
+exec { 'fix-wordpress-server-error':
+	command => 'sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php',
+	path    => '/usr/local/bin/:/bin/',
 }
