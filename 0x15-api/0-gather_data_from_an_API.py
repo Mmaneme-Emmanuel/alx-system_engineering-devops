6f8a29a07125e058734310429e80d8283b17889c
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-19 11:35:13 +0100

    task 0 done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+# Get the employee ID from command-line arguments
+employee_id = sys.argv[1]
+
+# Fetch employee details
+url = "https://jsonplaceholder.typicode.com/"  # Assuming this is the base URL
+response = requests.get(url + "users/{}".format(employee_id))
+employee = response.json()
+
+# Fetch todos for the employee
+todo_response = requests.get(url + "todos?userId={}".format(employee_id))
+todos = todo_response.json()
+
+# Collect completed tasks
+completed = []
+
+for todo in todos:
+    if todo.get("completed"):
+        completed.append(todo)
+
+# Print the result
+print("{} is done with {}/{}".format(employee['name'],
+      len(completed), len(todos)))
+
+for task in completed:
+    print("\t {}".format(task['title']))
