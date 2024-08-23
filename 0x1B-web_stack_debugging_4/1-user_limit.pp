54b5b1af2f8fc6fb6621e070014fc0c249952814
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-23 14:59:50 +0100

    task 1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -5,6 +5,7 @@ exec { 'increase-hard-file-limit-holberton-user':
   path    => ['/usr/local/bin', '/bin'],
 }
 
+# Increade security limit
 exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin'],
