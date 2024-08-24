7d401d11e3db3723a7a5691300bbc82360acd162
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-24 19:29:37 +0100

    task 1 corrected

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,11 @@
-# Increase the hard and soft file limits for the 'holberton' user
-
+# Increase the hard file limit for the 'holberton' user
 exec { 'increase-hard-file-limit-holberton-user':
-  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  command => 'sed -i "/^holberton hard nofile/s/^[0-9]*\s\+[0-9]*\s\+/\nholberton hard nofile 50000\n/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin'],
 }
 
-# Increade security limit
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+# Increase the soft file limit for the 'holberton' user
+exec { 'increase-soft-file-limit-holberton-user':
+  command => 'sed -i "/^holberton soft nofile/s/^[0-9]*\s\+[0-9]*\s\+/\nholberton soft nofile 50000\n/" /etc/security/limits.conf',
   path    => ['/usr/local/bin', '/bin'],
 }
