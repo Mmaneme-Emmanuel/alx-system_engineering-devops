6c02cdc58a92e925e2d66150ef519eccbe442347
Mmaneme-Emmanuel <mmaneme.emmanuel@gmail.com>
2024-08-24 21:42:37 +0100

    task done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
-# Puppet code that fixes a wordpress web 5XX error  to 200 is ok
+# A Puppet code that fixes a wordpress web 5XX error  to 200 is ok
 
-#The mistyped .phpp has been editted in the /var/www/html/wp-settings.php file
+#Editting the mistyped .phpp to php in the /var/www/html/wp-settings.php file
 
 exec { 'fix-wordpress-server-error':
-	command => 'sed -i \'s/phpp/php/g\' /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin/:/bin/',
+    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+    path    => '/usr/bin/:/bin/',
 }
